5a4514814b01b58def70992ccf580dd8ba3f4f3d
DrMat009=8 <ogungbuyia@gmail.com>
2022-09-09 04:48:01 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,16 @@
 /*
  * File: 2-print_alphabet.c
+ *
  * Auth: Brennan D Baraban
+ *
  */
 #include <stdio.h>
 /**
+ *
  * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
+ *
  */
 int main(void)
 {char letter;
