cdc4d2a46fdf30446c4845e59f2f5e05d2eecceb
DrMat009=8 <ogungbuyia@gmail.com>
2022-09-09 05:04:28 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,6 +10,8 @@
  */
 int main(void)
 {char low;
-for (low = 'a'; low <= 'z'; low++) putchar(low); putchar('\n');
+for (low = 'a'; low <= 'z'; low++) 
+	putchar(low);
+putchar('\n');
 return (0);
 }
