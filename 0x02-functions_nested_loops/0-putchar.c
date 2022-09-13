fbb994976e39d38e07a376ba5bd0b06f005ee89d
DrMat009=8 <ogungbuyia@gmail.com>
2022-09-13 04:58:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,7 @@
+#include "holberton.h"
 #include <unistd.h>
-/** _putchar - writes the character c to stdout
+/**
+ * _putchar - writes the character c to stdout
  *
  * @c: The character to print
  *
@@ -11,5 +13,4 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-
 }
