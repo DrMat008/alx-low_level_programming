67ad95c1f52f6d1322def90f2c914d2ed5ad1c4d
DrMat008 <ogungbuyia@gmail.com>
2022-09-26 05:49:48 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,16 +14,13 @@ char *_strpbrk(char *s, char *accept)
 	int i, n;
 	
 	for (i = 0; s[i] != '\0'; i++)
-
 	{
 		for (n = 0; accept[n] != '\0'; n++)
-
 		{
 			if (s[i] == accept[n])
 				return (s + i);
 		}
-
 	}
-	return (NULL);
 
+	return (NULL);
 }
