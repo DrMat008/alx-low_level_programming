952573486b52b4367fbe44736b50f83082baf946
DrMat008 <ogungbuyia@gmail.com>
2022-10-20 05:18:06 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,15 +16,24 @@ size_t print_list(const list_t *h)
 		return (0);
 
 	while (h != NULL)
+
 	{
 		if (h->str == NULL)
+
 		{
+
 			printf("[%d] %s\n", 0, "(nil)");
+
 		}
+
 		else
+
 		{
+
 			printf("[%d] %s\n", h->len, h->str);
+
 		}
+
 		h = h->next;
 		count++;
 	}
